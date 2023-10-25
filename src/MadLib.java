63f6282450b48dca20c42b9f3825a4e7c3ce5493
陈乐章 <melodychen@finn-chen.milton.edu>
2023-10-25 08:39:37 -0400

    Initial commit

---
+++ src/MadLib.java
@@ -0,0 +1,13 @@
+public class MadLib {
+
+    public static void main(String[] args) { //type psvm to get this
+        new MadLib();
+    }
+
+    public MadLib() { //constructor method, doesn't need void, same name as class
+        System.out.println("hello world");
+
+    }
+
+
+}
